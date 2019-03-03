22a6809b20427bbf5912a8c761940c8b31b7e69f
yjfyjf <13265855311@qq.com>
2019-03-03 09:56:45 +0800

    第一次提交

---
+++ public/m/js/index.js
@@ -0,0 +1,23 @@
+$(function () {
+    var gallery = mui('.mui-slider');
+    gallery.slider({
+        interval: 1000 //自动轮播周期，若为0则不自动播放，默认为0；
+    });
+
+ 
+    // 2. MUI的初始化区域滚动
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
+
+    // 2.2 使用swiper初始化内容滚动
+    // var swiper = new Swiper('#main > .swiper-container', {
+    //     direction: 'vertical',
+    //     slidesPerView: 'auto',
+    //     freeMode: true,
+    //     scrollbar: {
+    //         el: '.swiper-scrollbar',
+    //     },
+    //     mousewheel: true,
+    // });
+})
